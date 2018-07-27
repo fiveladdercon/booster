daa712c0b1d801d64a0adb2c0c971a74ecb68cde
fiveladdercon <fiveladdercon@gmail.com>
2018-07-27 11:24:12 -0400

    Added comments

--- demo/sign.c
+++ demo/sign.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
 /*
- * Import the sign API
+ * Import the sign API.
+ *
+ * Note that this API has the private key hard-coded into it
+ * via including the warden_private_key.h, so it should not
+ * be reference in the public application.
  */
 extern int WARDEN_sign (const char *imessage, char *osignature);
 
 /*
- *
  * To simplify the demo, each *line* in the input file is signed,
  * and then the "signed line" is transmitted as two consecutive lines
  * for simplified message/signature separation in the application.
  * 
  * The correct approach is to sign all lines once as single message,
- * but that adds complexity beyond the scope of a simple demo.  
+ * but that adds complexity beyond the scope of a simple demo:  
  *
  * i.e. Buffering the entire file is an option, but so is signing 
- * line-by-line using the underlying EVP_Init/Sign/Final.  It may
- * be that the message is not even in a file.  And all options 
- * require a message/signature separator that the application can 
- * use to split the signed message into it's two parts...
- *
+ * line-by-line using the underlying EVP_Init/Sign/Final crypto calls.  
+ * It may even be that the message is not even in a file.  And all 
+ * options require a message/signature separator that will not occur
+ * in the message that the application can reliably use to split 
+ * the signed message into it's two parts...
  */
 int main () {
 	char msg[513];
--- demo/verify.c
+++ demo/verify.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 /*
- * Import the verify API
+ * Import the verify API.
+ *
+ * Note that this API has the public key hard-coded into it via
+ * including the warden_public_key.h.
+ *
+ * It should also go without saying that despite the best efforts,
+ * authorization boils down to a boolean, which can be hijacked
+ * by the initiated:
+ *
+ * e.g. replacing the extern declaration below with:
+ *
+ * int WARDEN_verify (const char *imessage, const char *isignature) { return 1; };
+ *
+ * and recompiling, or acheiving the same effect with a couple of carefully
+ * inserted processor instructions.
  */
 extern int WARDEN_verify (const char *imessage, const char *isignature);
 
 /*
  * Split the signed message into message and signature by reading lines 
- * pairwise.
+ * pairwise.  To simplify the demo the separator is the newline character,
+ * but normally the separator needs to be a pattern that will not be
+ * in the message. 
  */
 int main () {
 	char msg[513];
